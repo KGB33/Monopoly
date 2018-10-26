1d05a19f3b7a0e89b0f9e37d6ddf571db4b14c8d
KGB33 <33pigbass@gmail.com>
2018-10-25 23:27:29 -0500

    Added Main function in Monoploy.py
    Need to fix bug that appears when rolling dice while in jail

--- Monopoly/Monopoly.py
+++ Monopoly/Monopoly.py
@@ -1,1 +1,19 @@
+from Player import Player
+from Board import Board
+"""
+Temp main to demonstrat / play with player class
+"""
 
+def __main__():
+    number_of_players = int(input("How many Players?"))
+    player_list = []
+    for i in range(0,number_of_players):
+        name = str(input("What is the next player's name?"))
+        player_list += [Player(name)]
+    for turn_number in range(0,4):
+        for i in range(0, number_of_players):
+            player_list[i].roll_dice()
+            print("\nPlayer: " + player_list[i].name + " is On " + Board.spaces[player_list[i].position])
+       
+
+__main__()
